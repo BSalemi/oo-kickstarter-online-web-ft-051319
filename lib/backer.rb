2be99fe685822271ddabbde2755296e35a2909ea
Brian Salemi <briandsalemi@gmail.com>
2019-06-02 15:32:26 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,11 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
+<<<<<<< HEAD
     project.add_backer(self)
+=======
+    project.add_backer
+>>>>>>> d7efab830dcc3c2e27353dd3d13fa1f7185e980e
   end
 
 
--- lib/project.rb
+++ lib/project.rb
@@ -9,6 +9,16 @@ class Project
 
   def add_backer(backer)
     @backers << backer
+<<<<<<< HEAD
     backer.backed_projects << self
   end
+=======
+    backer.back_project 
+  end
+
+
+
+
+
+>>>>>>> d7efab830dcc3c2e27353dd3d13fa1f7185e980e
 end
